cdf4275095ff31bd9b7d713e87e97a3d1ba93e91
Shashank0606 <shashankrathore606@gmail.com>
2022-12-04 22:53:31 +0530

    REFACTOR: Throw Custom Exception for Null Or Empty

--- moodanalyzer/src/main/java/com/bridgelabz/moodanalyzer/Moodanalyzer.java
+++ moodanalyzer/src/main/java/com/bridgelabz/moodanalyzer/Moodanalyzer.java
@@ -1,26 +1,26 @@
 package com.bridgelabz.moodanalyzer;
 
-public class Moodanalyzer {
-	private String message;
-
-	// Default Constructor
-	public Moodanalyzer() {
-	}
+public class MoodAnalyzer {
+	public String message;
 
 	// Parameterized Constructor
 	public Moodanalyzer(String message) {
 		this.message = message;
 	}
 
-	// Handle NULLPOINTER Exception using try-catch block
-	public String analyseMood() {
+	// Handle Customized Exception using try-catch block
+	public String analyseMood() throws MoodAnalysisException {
 		try {
+			if (message.length() == 0)
+				throw new MoodAnalysisException(MoodAnalysisException.ExceptionType.ENTERED_EMPTY,
+						"Please enter proper message!");
 			if (message.contains("sad"))
 				return "SAD";
 			else
 				return "HAPPY";
-		} catch (Exception e) {
-			return "HAPPY";
+		} catch (NullPointerException e) {
+			throw new MoodAnalysisException(MoodAnalysisException.ExceptionType.ENTERED_NULL,
+					"Please enter proper message!");
 		}
 	}
 }
