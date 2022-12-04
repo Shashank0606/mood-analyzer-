77c862a62c7b8c1479bec10b5b236640584fb285
Shashank0606 <shashankrathore606@gmail.com>
2022-12-04 22:06:20 +0530

    ADD: Ability to analyse and respond Happy or Sad mood

---
+++ moodanalyser/src/main/java/com/bridgelabz/moodanalyser/MoodanalyserApplication.java
@@ -0,0 +1,11 @@
+package com.bridgelabz.moodanalyser;
+
+public class MoodAnalyzer {
+	public String analyseMood(String message) {
+
+		if (message.contains("happy"))
+			return "HAPPY";
+		else
+			return "SAD";
+	}
+}
